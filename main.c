f39c7de482bade7e74ed2bba8ef792b7673d9526
x10dead <i@me.com>
2026-06-03 16:28:57 +0200

    trulalelo trulala trulimeru trulichina blablabla hahaha

--- main.c
+++ main.c
@@ -164,12 +164,12 @@ void	ft_strlcpy_test(void)
 {
 	LOG_DEBUG();
 	char	src[] = "shawarman"; /*src_len = 9*/
-	char	dst[10] = "4242424242"; /*dst_len = 10*/
+	char	dst[11] = "4242424242"; /*dst_len = 10*/
 	size_t	src_len = ft_strlen(src);
 	/* #1 */ check(ft_strlcpy(dst, src, 0) == src_len && dst[1] == '2'); 
 	/* #2 */ check(ft_strlcpy(dst, src, 1) == src_len && dst[1] == '2' && dst[0] != '4');
 	/* #3 */ check(ft_strlcpy(dst, src, 2) == src_len && dst[1] == '\0' && dst[0] == 's');
-	/* #4 */ check(ft_strlcpy(dst, src, -1) == src_len && !strcmp(src, dst) && dst[src_len + 1] == 's');ft_memset(dst, '6', 10); 
+	/* #4 */ check(ft_strlcpy(dst, src, -1) == src_len && !strcmp(src, dst) && dst[0] == 's');ft_memset(dst, '6', 10); 
 	/* #5 */ check(ft_strlcpy(dst, src, 6) == src_len && memcmp(dst, src, 6) && dst[5] == 0);
 	/* #6 */ check(ft_strlcpy(dst, "", 1) == 0 && !memcmp(dst, "", 1)); ft_memset(dst, '7', 10);
 	/* #7 */ check(ft_strlcpy(dst, "1", 0) == 1 && dst[0] == '7'); ft_memset(dst, '8', 10);
@@ -220,13 +220,137 @@ void	ft_strncmp_test(void)
 	printf("\n");
 }
 
+void	ft_strnstr_test(void)
+{
+	LOG_DEBUG();
+	char *haystack = "Death exists, not as the opposite but as a part of life";
+	/*				  01234567890123456789012345678901234567890123456789012345 */
+	char *needle = "as";
+	char *empty = "";
+	/* #1 */ check(ft_strnstr(haystack, needle, 0) == 0);
+	/* #2 */ check(ft_strnstr(haystack, needle, -1) == haystack +18);
+	/* #3 */ check(ft_strnstr(haystack, "a", -1) == haystack +2);
+	/* #4 */ check(ft_strnstr(haystack, "D", -1) == haystack);
+	/* #5 */ check(ft_strnstr(empty, "", -1) == empty);
+	/* #6 */ check(ft_strnstr("", empty, -1) == empty);
+	/* #7 */ check(ft_strnstr(haystack, "hot", -1) == NULL);
+	/* #8 */ check(ft_strnstr(haystack, "not", 14) == NULL);
+	/* #9 */ check(ft_strnstr(haystack, "not", 16) == NULL);
+	/* #10 */ check(ft_strnstr(haystack, "not", 17) == haystack +14);
+	/* #11 */ check(ft_strnstr(haystack, "got", 17) == NULL);
+	/* #12 */ check(ft_strnstr("got", "got", 4) == (char*)"got");
+	/* #13 */ check(ft_strnstr("got", "got", 3) == (char*)"got");
+	/* #14 */ check(ft_strnstr("got", "123", 0) == 0);
+	/* #15 */ check(ft_strnstr("got", "got", 2) == 0);
+	printf("\n");
+}
+
+void	ft_substr_test(void)
+{
+	LOG_DEBUG();
+	char *str = ft_substr("forty two", 3, -1);
+	/* #1 */ check(strncmp(str, "ty two", 6) == 0); 
+	str = ft_substr("forty two", 3, 0);
+	/* #2 */ check(strncmp(str, "", 10) == 0);
+	str = ft_substr("forty two", 11, 0);
+	/* #3 */ check(strncmp(str, "", 1) == 0);
+	str = ft_substr("Death exists, not as the opposite but as a part of life", 21, 12);
+	/*				 01234567890123456789012345678901234567890123456789012345*/
+	/* #4 */ check(strncmp(str, "the opposite", 14) == 0);
+	str = ft_substr("Death exists, not as the opposite but as a part of life", 21, -1);
+	/* #5 */ check(strncmp(str, "the opposite but as a part of life", 14) == 0);
+	str = ft_substr("Glory to Ukraine!", 9, 19);
+	/* #6 */ check(strncmp(str, "Ukraine!", 8) == 0);
+	str = ft_substr("Glory to Ukraine!", 16, 17);
+	/* #7 */ check(strncmp(str, "!", 1) == 0);
+	printf("\n");
+}
+
+void	ft_strjoin_test(void)
+{
+	LOG_DEBUG();
+	char *str = ft_strjoin("I love", " 42");
+	/* #1 */ check(strncmp(str, "I love 42", 9) == 0); 
+	str = ft_strjoin("", " 42");
+	/* #2 */ check(strncmp(str, " 42", 3) == 0);
+	str = ft_strjoin("42 ", "");
+	/* #3 */ check(strncmp(str, "42 ", 1) == 0);
+	str = ft_strjoin("", "");
+	/* #4 */ check(strncmp(str, "", 1) == 0);
+	str = ft_strjoin(" ", "");
+	/* #5 */ check(strncmp(str, " ", 1) == 0);
+	printf("\n");
+}
+
+void	ft_strtrim_test(void)
+{
+	LOG_DEBUG();
+	char *str = ft_strtrim("xxxmiroslauxxx", "x");
+	/* #1 */ check(strncmp(str, "miroslau", 8) == 0);
+	str = ft_strtrim("xxxmiroslauxxx", "");
+	/* #2 */ check(strncmp(str, "xxxmiroslauxxx", 14) == 0);
+	str = ft_strtrim("xxxmiroslauxxx", "xxx");
+	/* #3 */ check(strncmp(str, "miroslau", 8) == 0);
+	str = ft_strtrim(" x x x miroslau x x x ", " x ");
+	/* #4 */ check(strncmp(str, "miroslau", 8) == 0);
+	str = ft_strtrim(" x x x miroslau x x x ", " + ");
+	/* #5 */ check(strncmp(str, "x x x miroslau x x x", 19) == 0);
+	str = ft_strtrim(" xx xx x xxxx ", " x");
+	/* #6 */ check(strncmp(str, "", 1) == 0);
+	printf("\n");
+}
+
+void	ft_split_test(void)
+{
+	LOG_DEBUG();
+	/* #1 */ 
+	char *str = ">>> SPLIT WILL BE IMPLEMENTED WITH MEMORY MCHECK <<<";
+	printf(RED "\n%s\n", str);
+	printf("\n");
+}
+
+
+void	iter_helper_toUp(unsigned int i, char *str)
+{
+	((void) i);
+	if (*str >= 97 && *str <= 122)
+		*str -= 32;
+}
+
+void	iter_helper_toLower(unsigned int i, char *str)
+{
+	((void) i);
+	if (*str >= 65 && *str <= 90)
+		*str += 32;
+	else
+		return ;
+}
+
+
+void	ft_striteri_test(void)
+{
+	LOG_DEBUG();
+	char empty[] = ""; 
+	char str[] = "DeaTh Ei$tS, Not @$ thE 0pp0$ite but as a part of Life"; 
+	char str1[] = "DeaTh Ei$tS, Not @$ thE 0pp0$ite but as a part of Life"; 
+	char strUpper[] = "DEATH EI$TS, NOT @$ THE 0PP0$ITE BUT AS A PART OF LIFE"; 
+	char strLower[] = "death ei$ts, not @$ the 0pp0$ite but as a part of life"; 
+	ft_striteri(str, iter_helper_toUp);
+	/* #1 */ check(strncmp(str, strUpper, sizeof(str)) == 0); 
+	ft_striteri(str1, iter_helper_toLower);
+	/* #2 */ check(strncmp(str1, strLower, sizeof(str1)) == 0); 
+	ft_striteri(empty, iter_helper_toLower);
+	/* #3 */ check(strncmp("", empty, 1) == 0);
+ 	printf("\n");
+}
 
 
 /* void	ft_|_test(void) */
 /* { */
 /* 	LOG_DEBUG(); */
 /* 	/1* #1 *1/ */ 
-/*  printf("\n"); */
+/*  	printf("\n%s\n", str); */
+/*  	printf("\n"); */
 /* } */
 
 int	main(void)
@@ -244,4 +368,9 @@ int	main(void)
 		ft_strchr_test();
 		ft_strrchr_test();
 		ft_strncmp_test();
+		ft_strnstr_test();
+		ft_substr_test();
+		ft_strtrim_test();
+		ft_split_test();
+		ft_striteri_test();
 }
