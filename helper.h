c8dcffb7b506a0fb4a12aa085e897a50034f0ba5
roy <robert@666.support>
2026-06-29 23:00:46 +0200

    refactored helper.h

--- helper.h
+++ helper.h
@@ -1,35 +1,26 @@
-#ifndef LOGGER_H
-#define LOGGER_H
-#ifndef FAILURE_
-#define FAILURE_H
-#ifndef SUCCESS_H
-#define SUCCESS_H
-#ifndef CHECK_H
+# define LOGGER_H
+# define FAILURE_H
+# define SUCCESS_H
+# define CHECK_H
 
-#define CHECK_H
-#define CYAN    "\033[36m"
-#define RED     "\033[1m\033[31m"
-#define GREEN   "\033[32m"
-#define WHITE   "\033[37m"
-#define RESET   "\033[0m"
-#define YELLOW 	"\x1b[33m"
+# define CYAN	"\033[36m"
+# define RED	"\033[1m\033[31m"
+# define GREEN	"\033[32m"
+# define WHITE	"\033[37m"
+# define RESET	"\033[0m"
+# define YELLOW	"\x1b[33m"
 
-#include <stdio.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <unistd.h>
 
-#define LOG_DEBUG() do { \
+# define LOG_DEBUG() do { \
     printf(CYAN ">>> "WHITE "%s():\t" RESET,  __func__); \
 } while(0)
 
-#define SUCCESS(x)do { \
+# define SUCCESS(x)do { \
     printf( GREEN x RESET); \
 } while (0)
 
-#define FAILURE(x) do { \
+# define FAILURE(x) do { \
     printf( RED x RESET); \
 } while(0)
-
-#endif
-#endif
-#endif
-#endif
